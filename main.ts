9cea82d98d4077768e95b13b60b0caa10b08f422
maxanic <maxanic.weber@gmx.de>
2025-03-15 10:49:21 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,19 @@
+function Display (x: number, y: number, Text: string) {
+    if (y == 2) {
+        Y = 0
+        X = x + 20
+    } else if (y == 3) {
+        Y = 1
+        X = x + 20
+    } else {
+        Y = y
+        X = x
+    }
+    I2C_LCD1602.ShowString(Text, X, Y)
+}
+let X = 0
+let Y = 0
+I2C_LCD1602.LcdInit(39)
+basic.forever(function () {
+	
+})
